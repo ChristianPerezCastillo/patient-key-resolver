1cf51bc728b8e70019e202073d00977b27576c21
Christian <ch.hristian96@gmail.com>
2026-02-23 15:41:33 -0600

    Se crea el proyecto

---
+++ src/main/java/com/eleonor/patient_key_resolver/PatientKeyResolverApplication.java
@@ -0,0 +1,13 @@
+package com.eleonor.patient_key_resolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientKeyResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientKeyResolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eleonor/patient_key_resolver/PatientKeyResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleonor.patient_key_resolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientKeyResolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
