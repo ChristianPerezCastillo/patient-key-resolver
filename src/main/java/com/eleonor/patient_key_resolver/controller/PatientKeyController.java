af9bca27880ff65a40a879d56986f12bca04501e
Carlos Corona <ccorona@overcome.mx>
2026-02-27 12:14:43 -0600

    Imports

--- src/main/java/com/eleonor/patient_key_resolver/controller/PatientKeyController.java
+++ src/main/java/com/eleonor/patient_key_resolver/controller/PatientKeyController.java
@@ -1,13 +1,19 @@
 package com.eleonor.patient_key_resolver.controller;
 
-import com.googlecode.objectify.Key;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.googlecode.objectify.Key;
 
 @RestController
 @RequestMapping("/patient/resolve-key")
